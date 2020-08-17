9961c46071edcbba8727fac8d59e8e9dec4bd3ed
Jonathan Clem <jonathan@jclem.net>
2020-08-17 13:52:29 -0400

    Add settings for ex_doc

--- mix.exs
+++ mix.exs
@@ -1,13 +1,25 @@
 defmodule Sidecar.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :sidecar,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Sidecar",
+      source_url: "https://github.com/jclem/sidecar",
+      homepage_url: "https://github.com/jclem/sidecar",
+      docs: [
+        main: "Sidecar",
+        extras: ["README.md", "LICENSE.md"],
+        source_ref: @version
+      ]
     ]
   end
 
@@ -21,6 +33,7 @@ defmodule Sidecar.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
