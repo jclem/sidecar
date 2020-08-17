5e6ac5c7b02ac690257efebab28d81a5cc87d010
Jonathan Clem <jonathan@jclem.net>
2020-08-17 14:45:41 -0400

    Add documentation improvements

--- lib/sidecar/supervisor.ex
+++ lib/sidecar/supervisor.ex
@@ -3,8 +3,30 @@ defmodule Sidecar.Supervisor do
   Supervises a list of sidecar processes
   """
 
+  @doc false
   use Supervisor
 
+  @typedoc """
+  Options used to start a list of sidecar processes.
+
+  - `processes` A keyword list of processes to start whose keys are unique
+    process identifiers, and whose values are `t:Sidecar.Process.init_opts/0`
+    lists
+  """
+  @type init_opts :: [processes: Keyword.t(Sidecar.Process.init_opts())]
+
+  @doc """
+  Start one or more supervised sidecar processes.
+
+  ## Examples
+
+  ```elixir
+  Sidecar.Supervisor.start_link(processes: [
+    ngrok: "ngrok http 4000"
+  ])
+  ```
+  """
+  @spec start_link(init_opts) :: Supervisor.on_start()
   def start_link(init_opts \\ []) do
     opts = Keyword.merge(Application.get_all_env(:sidecar), init_opts)
     Supervisor.start_link(__MODULE__, opts)
