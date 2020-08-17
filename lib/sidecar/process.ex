ad11f12877dfc52d54152a3b0b3d79e36f8ad0ee
Jonathan Clem <jonathan@jclem.net>
2020-08-17 14:45:41 -0400

    Add documentation improvements

--- lib/sidecar/process.ex
+++ lib/sidecar/process.ex
@@ -1,12 +1,31 @@
 defmodule Sidecar.Process do
   @moduledoc """
   Supervises a single sidecar process
+
+  It is recommended one uses `Sidecar.Supervisor` to run sidecar processes,
+  rather than using this module manually.
   """
 
+  @doc false
   use GenServer
 
   require Logger
 
+  @typedoc """
+  Options used to start a sidecar process
+
+  - `name` An identifier for the process
+  - `command` The command to run, which is passed to `Port.open/2` using
+    `{:spawn, command}`. If the command is a function, the function is
+    evaluated just before the sidecar process is started. Its return value will
+    be the command.
+  """
+  @type init_opts :: [name: atom, command: String.t() | (() -> String.t())]
+
+  @doc """
+  Start a supervised sidecar process.
+  """
+  @spec start_link(init_opts) :: GenServer.on_start()
   def start_link(init_opts) do
     GenServer.start_link(__MODULE__, init_opts)
   end
@@ -15,20 +34,18 @@ defmodule Sidecar.Process do
   def init(opts) do
     Logger.metadata(sidecar: Keyword.fetch!(opts, :name))
 
-    cmd = Keyword.fetch!(opts, :command)
+    command =
+      case Keyword.fetch!(opts, :command) do
+        command when is_function(command) ->
+          command.()
 
-    cmd =
-      case cmd do
-        cmd when is_function(cmd) ->
-          cmd.()
-
-        cmd when is_binary(cmd) ->
-          cmd
+        command when is_binary(command) ->
+          command
       end
 
     port =
       Port.open(
-        {:spawn, "#{Path.join([__DIR__, "..", "..", "portwrap.sh"])} #{cmd}"},
+        {:spawn, "#{Path.join([__DIR__, "..", "..", "portwrap.sh"])} #{command}"},
         [{:line, Keyword.get(opts, :line_length, 1024)}, :exit_status]
       )
 
